472d73c488b95eb19a9a69eeeccd69c85fa9c259
aelapierre <lapierre82@gmail.com>
2019-11-03 23:07:41 -0300

    Deshabilito Scheduling Spring

--- src/main/java/com/alanlapierre/solarsystem/SolarSystemApplication.java
+++ src/main/java/com/alanlapierre/solarsystem/SolarSystemApplication.java
@@ -3,12 +3,11 @@ package com.alanlapierre.solarsystem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
 @EnableJpaAuditing
-@EnableScheduling
+//@EnableScheduling
 public class SolarSystemApplication {
 
 	public static void main(String[] args) {
